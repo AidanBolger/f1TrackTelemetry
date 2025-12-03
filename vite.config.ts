7d5cd4a534d2a70f0e1700043702dc1f6a3678e4
Aidan Bolger <aidanbolger88@gmail.com>
2025-12-03 16:56:14 -0500

    Set base to correct repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Base path for GitHub Pages (project site). Change if you host under a different repo/user.
-  base: '/f1demonstration/',
+  base: '/f1TrackTelemetry/',
   plugins: [react()],
 })
